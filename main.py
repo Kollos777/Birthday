fcd3cb74140e754fd2f781e6fa0227004f7b60b8
Kollos777 <kolloslit@gmail.com>
2023-12-04 01:04:56 -0600

    homework done

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
+from collections import defaultdict
 from datetime import date, datetime
 
 
 def get_birthdays_per_week(users):
     # Реалізуйте тут домашнє завдання
-    return users
+
+    birthdays_per_week = defaultdict(list)
+    today = date.today()
+
+    for user in users:
+        user_birthday = user["birthday"].replace(year=today.year)
+        if user_birthday < today:
+            user_birthday = user_birthday.replace(year=today.year+1)
+        remains = user_birthday - today
+
+        if remains.days >= 7:
+            continue
+
+        birthday_day_of_week = user_birthday.strftime('%A')
+
+        if  birthday_day_of_week in ["Saturday","Sunday"]:
+            birthday_day_of_week = "Monday"
+        
+        birthdays_per_week[birthday_day_of_week].append(user["name"])
+        print(birthday_day_of_week)
+
+        
+        
+
+    return birthdays_per_week
 
 
 if __name__ == "__main__":
